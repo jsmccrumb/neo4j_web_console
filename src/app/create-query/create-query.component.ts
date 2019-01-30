79f962ca32c19a3f25442b643b5c7353c019dfaf
CALIBRE\Jacob.McCrumb <jsmccrumb@gmail.com>
2019-01-30 17:14:51 -0500

    Add dropdown for batch key and tabs

--- src/app/create-query/create-query.component.ts
+++ src/app/create-query/create-query.component.ts
@@ -1,16 +1,19 @@
 import { Input, Component, OnInit } from '@angular/core';
 import { Query } from '../query';
 import { QUERY_TYPES } from '../query-types';
+import { SUMMARY_COUNTERS } from '../summary-counters';
 
 @Component({
   selector: 'app-create-query',
   templateUrl: './create-query.component.html',
   styleUrls: ['./create-query.component.css']
 })
+
 export class CreateQueryComponent implements OnInit {
   @Input() formId: string;
   query: Query = new Query();
   queryTypes = QUERY_TYPES;
+  batchKeys = SUMMARY_COUNTERS.filter(x => x.validBatchKey);
 
   constructor() { }
 
---
+++ src/app/summary-counters.ts
@@ -0,0 +1,57 @@
+export const SUMMARY_COUNTERS: any[] = [
+  {
+  value: 'constraintsAdded',
+  viewValue: 'Constraints Added',
+  validBatchKey: false
+  },
+  {
+  value: 'constraintsRemoved',
+  viewValue: 'Constraints Removed',
+  validBatchKey: false
+  },
+  {
+  value: 'indexesAdded',
+  viewValue: 'Indexes Added',
+  validBatchKey: false
+  },
+  {
+  value: 'indexesRemoved',
+  viewValue: 'Indexes Removed',
+  validBatchKey: false
+  },
+  {
+  value: 'labelsAdded',
+  viewValue: 'Labels Added',
+  validBatchKey: true
+  },
+  {
+  value: 'labelsRemoved',
+  viewValue: 'Labels Removed',
+  validBatchKey: true
+  },
+  {
+  value: 'nodesCreated',
+  viewValue: 'Nodes Created',
+  validBatchKey: true
+  },
+  {
+  value: 'nodesDeleted',
+  viewValue: 'Nodes Deleted',
+  validBatchKey: true
+  },
+  {
+  value: 'propertiesSet',
+  viewValue: 'Properties Set',
+  validBatchKey: true
+  },
+  {
+  value: 'relationshipsCreated',
+  viewValue: 'Relationships Created',
+  validBatchKey: true
+  },
+  {
+  value: 'relationshipsDeleted',
+  viewValue: 'Relationships Deleted',
+  validBatchKey: true
+  }
+]
